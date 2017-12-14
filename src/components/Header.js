11e14547555eb297c160e8f30d0e7ad30b6f938e
peterchic <peterchicarielli@gmail.com>
2017-12-13 22:50:27 -0500

    style

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>BOLD.</h1>
+          <h1>ORIGINAL.</h1>
+          <h1>LONG LASTING.</h1>
+        </div>
+        <div className="hero-box">
+          <img className="amazon" src="http://www.acim.circleofa.org/wp-content/uploads/2017/07/amazon-logo_black.png" alt="buy online"/>
+        </div>
+    </div>
+  <div>
+    <p>Page Content..</p>
+  </div>
+</div>
+  );
+};
